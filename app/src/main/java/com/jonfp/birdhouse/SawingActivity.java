4b56f2823a74c808e3a503ce0121e4330ea5d492
jacobwetterholt <ja0542we-s@student.lu.se>
2024-03-26 16:49:20 +0100

    Slightly improved SawingActivity, shorter sawing sound effect,

--- app/src/main/java/com/jonfp/birdhouse/SawingActivity.java
+++ app/src/main/java/com/jonfp/birdhouse/SawingActivity.java
@@ -1,17 +1,30 @@
 package com.jonfp.birdhouse;
 
 import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class SawingActivity extends AccelerometerActivity {
 
     private boolean saw_is_extended = false;
     private int saw_movement_changes = 0;
-    private static final int MOVEMENT_THRESHOLD = 7;
-    private static final int movement_changes_THRESHOLD = 5;
+    private static final int MOVEMENT_THRESHOLD = 4;
+    private static final int movement_changes_THRESHOLD = 1;
+
+    private TextView textViewStatus;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sawing);
+        textViewStatus = findViewById(R.id.textViewSawingStatus);
+    }
+
 
     @Override
     protected void setupMedia() {
-        mediaPlayer = MediaPlayer.create(this, R.raw.saw);
+        mediaPlayer = MediaPlayer.create(this, R.raw.saw_short);
     }
 
     @Override
@@ -25,9 +38,12 @@ public class SawingActivity extends AccelerometerActivity {
         }
         if(saw_movement_changes > movement_changes_THRESHOLD) {
             System.out.println("YOU ARE SAWING");
+            textViewStatus.setText("Status: Sawing");
             if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
                 mediaPlayer.start();
             }
+            saw_movement_changes = 0;
+            textViewStatus.setText("Status: Not Sawing");
         }
     }
 }
