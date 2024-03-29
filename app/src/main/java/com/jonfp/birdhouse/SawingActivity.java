256e5b728bd4e00556c0c493ad7f3cd0e5867e4e
Signe Johansson <signe.johansson.5006@student.lu.se>
2024-03-29 17:40:26 +0100

    user is redirected to listening activity after sawing x amount of strokes

--- app/src/main/java/com/jonfp/birdhouse/SawingActivity.java
+++ app/src/main/java/com/jonfp/birdhouse/SawingActivity.java
@@ -1,5 +1,6 @@
 package com.jonfp.birdhouse;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -8,10 +9,12 @@ public class SawingActivity extends AccelerometerActivity {
 
     private boolean saw_is_extended = false;
     private int saw_movement_changes = 0;
-    private static final int MOVEMENT_THRESHOLD = 4;
+    private static final int MOVEMENT_THRESHOLD = 6;
     private static final int movement_changes_THRESHOLD = 1;
+    private static final int STROKE_COUNT_THRESHOLD = 10;
 
     private TextView textViewStatus;
+    private int strokeCount = 0;
 
 
     @Override
@@ -38,12 +41,22 @@ public class SawingActivity extends AccelerometerActivity {
         }
         if(saw_movement_changes > movement_changes_THRESHOLD) {
             System.out.println("YOU ARE SAWING");
+            strokeCount++;
             textViewStatus.setText("Status: Sawing");
             if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
                 mediaPlayer.start();
             }
+            if (strokeCount >= STROKE_COUNT_THRESHOLD) {
+                redirectToNewActivity();
+            }
             saw_movement_changes = 0;
             textViewStatus.setText("Status: Not Sawing");
         }
+    }
+
+    private void redirectToNewActivity() {
+        Intent intent = new Intent(this, ListeningActivity.class);
+        startActivity(intent);
+        finish(); // Finish current activity to prevent going back to it on back press
     }
 }
