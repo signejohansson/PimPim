07999674835aec19d53a4d76feaa0e19aa2c1171
jonfpersson <jonfpersson@gmail.com>
2024-05-14 09:55:11 +0200

    Add easter egg

--- app/src/main/java/com/jonfp/birdhouse/MainActivity.java
+++ app/src/main/java/com/jonfp/birdhouse/MainActivity.java
@@ -3,8 +3,10 @@ package com.jonfp.birdhouse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonprank).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // play video from drawable
+                VideoView videoView = new VideoView(MainActivity.this);
+                setContentView(videoView);
+
+                Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.output);
+                videoView.setVideoURI(videoUri);
+                videoView.start();
+            }
+        });
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
